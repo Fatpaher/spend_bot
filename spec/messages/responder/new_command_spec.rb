57a55a3cf8f77c8b100f6da6ecde667eba6f38c1
Paul Pavlovsky <fatpaher@gmail.com>
2019-05-12 15:54:30 +0300

    Split responder specs to separate files
    f

---
+++ spec/messages/responder/new_command_spec.rb
@@ -0,0 +1,92 @@
+require './spec/spec_helper'
+
+RSpec.describe '/new command' do
+  it 'creates new event' do
+    user = build_stubbed :user
+    message = FakeTelegramMessage.new(
+      from: user,
+      text: '/new 123 01.05.2019'
+    )
+    bot = double
+    sender = double Messages::Sender
+
+    options = {
+      bot: bot,
+      message: message,
+      user: user,
+      sender: sender,
+    }
+
+    allow(sender).to receive(:send)
+
+    expect do
+      Messages::Responder.new(options).respond
+    end.to change(Event, :count).by(1)
+
+    expect(Event.last).to have_attributes(
+      user_id: user.id,
+      amount: 123,
+      date: Date.parse('01.05.2019'),
+      category: '#other',
+    )
+
+    expected_text = "Expence added"
+    expect(sender).to have_received(:send).with(expected_text)
+  end
+
+  context 'without ammount' do
+    it 'returns error' do
+      user = build_stubbed :user
+      message = FakeTelegramMessage.new(
+        from: user,
+        text: '/new'
+      )
+      bot = double
+      sender = double Messages::Sender
+
+      options = {
+        bot: bot,
+        message: message,
+        user: user,
+        sender: sender,
+      }
+      allow(sender).to receive(:send)
+
+      expect do
+        Messages::Responder.new(options).respond
+      end.not_to change(Event, :count)
+
+      expected_text = 'Hey, you probably forgot to add ammount to your message'
+      expect(sender).to have_received(:send).with(expected_text)
+    end
+  end
+
+  context 'category options' do
+    it 'set category from tag ' do
+      user = build_stubbed :user
+      message = FakeTelegramMessage.new(
+        from: user,
+        text: '/new 123 #foo'
+      )
+      bot = double
+      sender = Messages::Sender
+
+      options = {
+        bot: bot,
+        message: message,
+        user: user,
+        sender: sender,
+      }
+
+      allow(sender).to receive(:send)
+
+      expect do
+        Messages::Responder.new(options).respond
+      end.to change(Event, :count).by(1)
+
+      expect(Event.last).to have_attributes(
+        category: '#foo'
+      )
+    end
+  end
+end
---
+++ spec/messages/responder/show_command_spec.rb
@@ -0,0 +1,87 @@
+require './spec/spec_helper'
+
+RSpec.describe '/show command' do
+  context 'no date present' do
+    it 'returns stat' do
+      user = build_stubbed :user
+      message = FakeTelegramMessage.new(
+        from: user,
+        text: '/show 123'
+      )
+      bot = double
+      sender = double Messages::Sender
+
+      create_list(
+        :event,
+        2,
+        user: user,
+        category: :food,
+        amount: 2,
+      )
+      create(
+        :event,
+        user: user,
+        category: :drink,
+        amount: 10,
+      )
+
+      options = {
+        bot: bot,
+        message: message,
+        user: user,
+        sender: sender,
+      }
+
+      allow(sender).to receive(:send)
+
+      Messages::Responder.new(options).respond
+
+      expected_text = [
+        "Expences for #{Date.current.strftime('%B %Y')}",
+        '#drink 10',
+        '#food 4',
+      ].join("\n")
+      expect(sender).to have_received(:send).with(expected_text)
+    end
+  end
+
+  context 'month present' do
+    it 'returns stat for matched month of current year' do
+      user = build_stubbed :user
+      message = FakeTelegramMessage.new(
+        from: user,
+        text: '/show 123 April'
+      )
+      bot = double
+      sender = double Messages::Sender
+      year = Date.current.year
+
+      ["02.04.#{year}", "30.03.#{year}"].each do |date|
+        create(
+          :event,
+          user: user,
+          category: :drink,
+          amount: 10,
+          date: date,
+        )
+      end
+
+      options = {
+        bot: bot,
+        message: message,
+        user: user,
+        sender: sender,
+      }
+
+      allow(sender).to receive(:send)
+
+      Messages::Responder.new(options).respond
+
+      expected_text = [
+        "Expences for April #{year}",
+        '#drink 10',
+      ].join("\n")
+      expect(sender).to have_received(:send).with(expected_text)
+    end
+  end
+end
